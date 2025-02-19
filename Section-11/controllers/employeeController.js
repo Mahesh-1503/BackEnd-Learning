5aadf70510355f9cd27f199a9385c5d0151f26d6
Mahesh-1503 <151714970+Mahesh-1503@users.noreply.github.com>
2025-02-19 16:49:18 +0530

    GET
    getting allEmp data also dynamically getting single emp data through unique ID from MongoDB document record
    completed

--- Section-11/controllers/employeeController.js
+++ Section-11/controllers/employeeController.js
@@ -1,5 +1,7 @@
 const Employee = require('../models/Employee')
 
+
+//POST Method Code
 const createEmployee = async(req,res) => {
     try{
         const {name, email, phone, city} = req.body
@@ -18,4 +20,31 @@ const createEmployee = async(req,res) => {
     }
 }
 
-module.exports = {createEmployee}
+//GET Method Code
+const getEmployees = async(req,res) => {
+    try{
+        const employees = await Employee.find()
+        res.status(200).json(employees)
+    }catch(error){
+        console.log(`There is an error: ${error}`)
+        res.status(500).json({message: "server error"})
+    }
+}
+
+//GET Method To GET Single Employee Data With ID
+const singleEmployee = async(req,res) => {
+    try{
+        const employee = await Employee.findById(req.params.id)
+
+        if(!employee){
+            return res.status(404).json({message: "Employee not found"})
+        }
+
+        res.status(200).json(employee)
+    }catch(error){
+        console.error(`there is an error: ${error} `)
+        res.status(500).json({message: `SERVER ERROR`})
+    }
+}
+
+module.exports = {createEmployee, getEmployees, singleEmployee}
--- Section-11/routes/employeeRoutes.js
+++ Section-11/routes/employeeRoutes.js
@@ -8,8 +8,9 @@ const Employee = require('../models/Employee')
 
 //get post put/patch delete
 
-router.post('/add-emp', employeeController.createEmployee)
-
+router.post('/add-emp', employeeController.createEmployee)//POST
+router.get('/allemployees', employeeController.getEmployees)//GET all emp DATA
+router.get('/employee/:id',employeeController.singleEmployee )//GET single emp DATA
 
 
 module.exports = router
